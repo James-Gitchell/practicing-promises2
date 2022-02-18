40b99fc3cc44906874a05705403940439ed94552
James Gitchell <jamesgitchell.dev@gmail.com>
2022-02-18 14:07:23 -0500

    merged with new folder

---
+++ promise.js
@@ -0,0 +1,17 @@
+let p = new Promise((resolve, reject) => {
+    let a = 1 + 5;
+    if(a === 2){
+        resolve("Success")
+    } else {
+        reject("Failed")
+    }
+})
+
+p.then((message) => {
+    console.log("this is in the then " + message);
+}).catch((message) => {
+    console.log("this is in the catch " + message)
+})
+
+
+
